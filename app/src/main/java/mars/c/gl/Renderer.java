c6ac942f25ddbf81f22aea06bbe13f569ed7eb89
Constantine Mars <constantine.mars@gmail.com>
2015-07-02 10:58:35 +0300

    shaders drawing (incomplete)

--- app/src/main/java/mars/c/gl/Renderer.java
+++ app/src/main/java/mars/c/gl/Renderer.java
@@ -1,5 +1,6 @@
 package mars.c.gl;
 
+import android.graphics.Color;
 import android.opengl.GLSurfaceView;
 
 import java.nio.ByteBuffer;
@@ -10,8 +11,14 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_LINES;
+import static android.opengl.GLES20.GL_POINTS;
+import static android.opengl.GLES20.GL_TRIANGLES;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glDrawElements;
+import static android.opengl.GLES20.glUniform4fv;
 import static android.opengl.GLES20.glUseProgram;
 import static android.opengl.GLES20.glViewport;
 
@@ -38,25 +45,28 @@ public class Renderer implements GLSurfaceView.Renderer {
             4.5f, 2f,
             4.5f, 12f
     };
+    private int color = Color.GREEN;
     private static final int FS=4;
     private final FloatBuffer vd;
     private int p;
+    private boolean v;
 
     public Renderer() {
         vd = ByteBuffer.allocateDirect(vs.length * FS)
         .order(ByteOrder.nativeOrder())
         .asFloatBuffer();
         vd.put(vs);
-
-        p=Shaders.linkProgram();
-        if(Shaders.validateProgram(p)){
-            glUseProgram(p);
-        }
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        glClearColor(1f,0f,0f,0f);
+        glClearColor(0f,0f,0f,0f);
+        p=Shaders.linkProgram();
+        if(Shaders.validateProgram(p)){
+            glUseProgram(p);
+            Shaders.setVertices(p, vd, VS);
+            v=true;
+        }
     }
 
     @Override
@@ -67,5 +77,13 @@ public class Renderer implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 gl) {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        if(v) {
+            Shaders.setColor(color);
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+            glDrawArrays(GL_LINES, 6, 2);
+            glDrawArrays(GL_POINTS, 8, 1);
+            glDrawArrays(GL_POINTS, 9, 1);
+        }
     }
 }
--- app/src/main/java/mars/c/gl/Shaders.java
+++ app/src/main/java/mars/c/gl/Shaders.java
@@ -1,8 +1,13 @@
 package mars.c.gl;
 
+import android.graphics.Color;
+
+import java.nio.FloatBuffer;
+
 import timber.log.Timber;
 
 import static android.opengl.GLES20.GL_COMPILE_STATUS;
+import static android.opengl.GLES20.GL_FLOAT;
 import static android.opengl.GLES20.GL_FRAGMENT_SHADER;
 import static android.opengl.GLES20.GL_LINK_STATUS;
 import static android.opengl.GLES20.GL_VALIDATE_STATUS;
@@ -12,31 +17,38 @@ import static android.opengl.GLES20.glCompileShader;
 import static android.opengl.GLES20.glCreateProgram;
 import static android.opengl.GLES20.glCreateShader;
 import static android.opengl.GLES20.glDeleteShader;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetAttribLocation;
 import static android.opengl.GLES20.glGetProgramInfoLog;
 import static android.opengl.GLES20.glGetProgramiv;
 import static android.opengl.GLES20.glGetShaderInfoLog;
 import static android.opengl.GLES20.glGetShaderiv;
+import static android.opengl.GLES20.glGetUniformLocation;
 import static android.opengl.GLES20.glLinkProgram;
 import static android.opengl.GLES20.glShaderSource;
+import static android.opengl.GLES20.glUniform4f;
 import static android.opengl.GLES20.glValidateProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
 
 /**
  * Created by Constantine Mars on 6/29/15.
  */
 public class Shaders {
-    public static final String AP ="ap";
-    public static final String UC ="uc";
+    public static final String POSITION ="ap";
+    public static final String COLOR ="uc";
     private static final String VERTEX_SHADER="" +
-            "attribute vec4 "+ AP +
+            "attribute vec4 "+ POSITION + ";"+
             "void main() {" +
-            "   gl_Position="+ AP +
+            "   gl_Position="+ POSITION + ";"+
             "}";
     private static final String FRAGMENT_SHADER="" +
-            "precision mediump float" +
-            "uniform vec4 "+ UC +
+            "precision mediump float" + ";"+
+            "uniform vec4 "+ COLOR + ";"+
             "void main() {" +
-            "   gl_FragColor="+ UC +
+            "   gl_FragColor="+ COLOR + ";"+
             "}";
+    private static Integer uColor;
+    private static Integer aPos;
     public static int compileVertexShader(){
         return compileShader(GL_VERTEX_SHADER, VERTEX_SHADER);
     }
@@ -49,6 +61,7 @@ public class Shaders {
         int s=glCreateShader(t);
         if(s==0){
             Timber.e("Couldn't create shader");
+            return 0;
         }
         glShaderSource(s, c);
         glCompileShader(s);
@@ -79,6 +92,10 @@ public class Shaders {
             Timber.e("Link program failed: " + glGetProgramInfoLog(p));
             return 0;
         }
+
+        uColor=glGetUniformLocation(p, COLOR);
+        aPos=glGetAttribLocation(p, POSITION);
+
         return p;
     }
 
@@ -88,6 +105,16 @@ public class Shaders {
         glGetProgramiv(p, GL_VALIDATE_STATUS, vs, 0);
         Timber.v("Validating program result: " + vs[0] + " - " + glGetProgramInfoLog(p));
         return vs[0] != 0;
+    }
+
+    public static void setVertices(int p, FloatBuffer vd, int vs){
+        vd.position(0);
+        glVertexAttribPointer(aPos, vs, GL_FLOAT, false, 0, vd);
+        glEnableVertexAttribArray(aPos);
+    }
+
+    public static void setColor(int c){
+        glUniform4f(uColor, Color.red(c), Color.green(c), Color.blue(c), 0f);
     }
 
 }
